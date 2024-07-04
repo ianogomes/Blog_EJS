fbf11bd411a9f74b82499bd212522fca3eeba97a
ianogomes <ianoligomes98@gmail.com>
2024-07-04 13:06:13 -0300

    Add Data Persistence with Postgres

--- README.md
+++ README.md
@@ -1,11 +1,31 @@
 # Blog_EJS
-The primary goal of this project is simply practicing my back-end skills using node, express and ejs initially. In this case, the website I am developing is a blog, and the project is based on a challenge from the following web development course: https://www.udemy.com/course/the-complete-web-development-bootcamp/
+The primary goal of this project was simply practicing my back-end skills using node, express and ejs initially, then adding data persistence with PostgreSQL. In this case, the website I am developing is a blog, and the project is based on a challenge from the following web development course: https://www.udemy.com/course/the-complete-web-development-bootcamp/
 
-Later on, I will probably expand this project to practice my skills with databases too.
-
-## Desired Features
+## Features
 1. Post Creation: Users should be able to create new posts.
 2. Post Viewing: The home page should allow the user to view all their posts.
 3. Post Update/Delete: Users should be edit and delete posts as needed.
 4. Styling: The application should be well-styled and responsive, ensuring a good user 
 experience on both desktop and mobile devices.
+
+## Installation
+
+```bash
+$ npm install
+```
+
+## Database
+
+You should add your database access information in a .env file created by you before running the app.
+
+## Running the app
+
+```bash
+# development
+$ node index.js
+
+# watch mode
+$ npm i -g nodemon #only-once installation
+$ nodemon index.js
+```
+The app is accesed in http://localhost:3000/
--- index.js
+++ index.js
@@ -1,57 +1,98 @@
 import express from "express";
 import bodyParser from "body-parser";
-import multer from "multer";
+import pg from "pg";
+import env from "dotenv";
 
 const app = express();
 const port = 3000;
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
-const upload = multer()
+env.config();
 
-const postList = new Object();
+const db = new pg.Client({
+    user: process.env.PG_USER,
+	host: process.env.PG_HOST,
+	database: process.env.PG_DATABASE,
+	password: process.env.PG_PASSWORD,
+	port: process.env.PG_PORT
+  });
+  db.connect();
 
-const random = () => Math.random().toString(36).slice(-5);
-postList[random()] = ["title1", "Lorem ipsum dolor sit amet, et proin, justo mus. Porta suspendisse turpis netus sagittis tortor at, vitae aliquetpharetra cras velit, id gravida, neque nulla lorem, posuere hac. Ultricies eget lacus vehicula, in ante aliquamet,facilisis tempor duis orci. Sed molestie sem in duis eu id, nisl id ultricies metus blandit eget praesent, pedetempornullam, vitae arcu tortor suspendisse nibh risus. Nulla id suscipit reiciendis nulla erat. Porta enim auteluctus,ducimus sodales dolor."];
-postList[random()] = ["title2", "text2"];
-
-app.get("/", (req, res) => {
-    res.render("index.ejs", {list: postList});
+// HOME
+app.get("/", async (req, res) => {
+    const result = await db.query("SELECT * FROM posts ORDER BY id");
+    let posts = result.rows;
+    res.render("index.ejs", {list: posts});
 });
 
+// WRITING AREA
 app.get("/create", (req, res) => {
     res.render("create.ejs", {title: "", text: "", key: ""});
 });
 
-app.post("/submit", (req, res) => {
-    postList[random()] = [req.body.title, req.body.text];
-    res.render("index.ejs", {list: postList});
+// SAVING NEW POST
+app.post("/submit", async (req, res) => {
+    try{
+        const result = await db.query("INSERT INTO posts (title, text) VALUES ($1, $2) RETURNING *",[req.body.title, req.body.text]);
+        const id = result.rows[0].id;
+        res.redirect("/"+String(id));
+    } catch (err) {
+        console.log(err);
+    }
 });
 
-for (const key of Object.keys(postList)){
-    let link = "/" + key;
-    let link_delete = "/delete" + key;
-    let link_update = "/create" + key;
-    let link_submit = "/submit" + key;
+// POST
+app.get("/:id", async (req, res) => {
+    try {
+        const id = parseInt(req.params.id);
+        // console.log(id);
+        const result = await db.query("SELECT * FROM posts WHERE id = $1",[id]);
+        let post = result.rows[0];
+        // console.log(post);
+        if (post) {
+            res.render("post.ejs", {post: post, key: String(id)});
+        } else {
+            throw new SyntaxError("Custom error: The post id is not registered");
+        }
+    } catch (err) {
+        console.log(err);
+        res.redirect("/");
+    }
+    
+});
 
-	app.get(link, (req, res) => {
-        res.render("post.ejs", {list: postList, key: key});
-    });
+// DELETING
+app.get("/delete/:id", async (req, res) => {
+    const id = parseInt(req.params.id);
+    try{
+        await db.query("DELETE FROM posts WHERE id = $1",[id]);
+        res.redirect("/");
+    } catch (err) {
+        console.log(err);
+    }
+});
 
-    app.get(link_delete, (req, res) => {
-        delete postList[key];
-        res.render("index.ejs", {list: postList});
-    });
+// WRITING AREA FOR UPDATING
+app.get("/create/:id", async (req, res) => {
+    const id = parseInt(req.params.id);
+    const result = await db.query("SELECT * FROM posts WHERE id = $1",[id]);
+    let post = result.rows[0];
 
-    app.get(link_update, (req, res) => {
-        res.render("create.ejs", {title: postList[key][0], text: postList[key][1], key: key});
-    });
+    res.render("create.ejs", {title: post.title, text: post.text, key: post.id});
+});
 
-    app.post(link_submit, (req, res) => {
-        postList[key] = [req.body.title, req.body.text]
-        res.render("post.ejs", {list: postList, key: key});
-    });
-}
+// SAVING UPDATE
+app.post("/submit/:id", async (req, res) => {
+    const id = parseInt(req.params.id);
+
+    try{
+        await db.query("UPDATE posts SET title = $1, text =$2 WHERE id = $3",[req.body.title, req.body.text, id]);
+        res.redirect("/"+String(id));
+    } catch (err) {
+        console.log(err);
+    }
+});
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
